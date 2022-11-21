517f4137cf88883dd92ab88215877c95b238414d
Frank <praisefranklin005@gmail.com>
2022-11-21 16:06:50 +0100

    added extra files for pythoneverywhere deployment reconsiliation

--- PrepUp/settings.py
+++ PrepUp/settings.py
@@ -21,21 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-egsyyrs)#o@z0&d44p8kias-2qo#^8rd0@2^h83&vh8@+6m1ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-SECRET_KEY = 'django-insecure-egsyyrs)#o@z0&d44p8kias-2qo#^8rd0@2^h83&vh8@+6m1ai'
 
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-
-
-# from .env import SECRET_KEY
-
-# from django.core.management.utils import get_random_secret_key
-
-# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -129,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
